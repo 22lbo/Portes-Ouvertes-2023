3831910b3cf91eaa805d44aad1946add40775f62
Lucas Borter <lucas.borter@ceff.ch>
2023-11-10 13:06:28 +0100

    Solution SimpleR_Hub - Supprimé
    Elimination de la solution visual code

--- SignalR_Hub/SignalR_Hub/DrawingHub.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.SignalR;
-
-namespace SignalR_Hub
-{
-    public class DrawingHub : Hub
-    {
-        public async Task SendMessage(string user, string message) => await Clients.All.SendAsync("ReceiveMessage", user, message);
-    }
-}
