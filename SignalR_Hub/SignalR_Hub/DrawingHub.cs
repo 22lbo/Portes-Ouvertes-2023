027b43e6b486ef5b3d69d4b1e15a8cef8e5873d2
Lucas Borter <lucas.borter@ceff.ch>
2023-11-10 10:01:03 +0100

    Signal R

---
+++ SignalR_Hub/SignalR_Hub/DrawingHub.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalR_Hub
+{
+    public class DrawingHub : Hub
+    {
+        public async Task SendMessage(string user, string message) => await Clients.All.SendAsync("ReceiveMessage", user, message);
+    }
+}
