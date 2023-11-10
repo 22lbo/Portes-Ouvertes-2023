ece629b961bc50a96f4fc70b150558db44834846
Borter Lucas <Lucas.Borter@ceff.ch>
2023-11-10 07:46:48 +0100

    Programme de dessin

---
+++ SimpleDraw/SimpleDraw/Line.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleDraw
+{
+    public class Line : Shape
+    {
+        public override void Draw(Graphics g)
+        {
+            g.DrawLine(Pen, X1, Y1, X2, Y2);
+        }
+    }
+}
---
+++ SimpleDraw/SimpleDraw/Rectangle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleDraw
+{
+    public class Rectangle : Shape
+    {
+        public override void Draw(Graphics g)
+        {
+            g.DrawRectangle(Pen, Math.Min(X1,X2), Math.Min(Y1, Y2), Math.Abs(X2-X1), Math.Abs(Y2-Y1));
+        }
+    }
+}
